41b598e7e7e30b58c0840224a99f15a8d9ae3361
manisha guda <manishavguda@gmail.com>
2023-03-09 18:16:54 +0530

    Create Server Script

---
+++ 05-function.sh
@@ -0,0 +1,12 @@
+# Function are like variables
+# Variable - When we assign to a set of data then it is a variable.
+# Function - When we assign to a set of data then it is a function.
+
+
+# Function Declare
+example() {
+  echo I am a example function
+}
+
+##call a function
+example
