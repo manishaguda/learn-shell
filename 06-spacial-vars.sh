5e0affa1b167e351c383fa1b53addad8201c20d9
manisha guda <manishavguda@gmail.com>
2023-03-09 21:51:29 +0530

    Create Server Script

---
+++ 06-spacial-vars.sh
@@ -0,0 +1,18 @@
+echo "Following values from script"
+echo "0 - $0"
+echo "1 - $1"
+echo "2 - $2"
+echo "* - $*"
+echo "@ - $@"
+echo "# - $#"
+
+example() {
+  echo "0 - $0" # not applicable for function
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+
+example xyz
